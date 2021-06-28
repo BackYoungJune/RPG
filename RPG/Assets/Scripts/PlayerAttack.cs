ca93b2aec6d887f7731457c7f0c013f1824c7623
BackYoungJune <74597441+BackYoungJune@users.noreply.github.com>
2021-06-28 13:01:09 +0900

    aa

---
+++ RPG/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public enum STATE
+    {
+        NORMAL, ATTACK, SKILL
+    }
+    public STATE myState = STATE.NORMAL;
+
+    public Animator myAnim;
+    public PlayerAnimEvent myAnimEvent;
+    public LayerMask ClickMask;
+    public float AttackTime;
+
+    private void Awake()
+    {
+        myAnim = GetComponent<Animator>();
+        myAnimEvent = GetComponent<PlayerAnimEvent>();
+    }
+
+    private void Start()
+    {
+        myAnimEvent.Attack += OnAttack;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+            //if (Physics.Raycast(ray, out hit, 999.0f, ClickMask))
+            //{
+            //    if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            //    {
+            //        Target = hit.point;
+            //        ChangeState(STATE.BATTLE);
+            //    }
+            //    else
+            //    {
+            //        Target = hit.point;
+            //        ChangeState(STATE.NORMAL);
+            //    }
+            //}
+        }
+
+        StateProcess();
+    }
+
+    void ChangeState(STATE s)
+    {
+        if (myState == s) return;
+        myState = s;
+
+        switch (myState)
+        {
+            case STATE.NORMAL:
+                {
+                    break;
+                }
+            case STATE.ATTACK:
+                {
+                    myAnim.SetTrigger("Attack");
+                    break;
+                }
+            case STATE.SKILL:
+                {
+                    break;
+                }
+        }
+    }
+
+    void StateProcess()
+    {
+        switch (myState)
+        {
+            case STATE.NORMAL:
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ChangeState(STATE.ATTACK);
+                    }
+
+                    break;
+                }
+            case STATE.ATTACK:
+                {
+                    //if (Target != Vector3.zero)
+                    //{
+                    //    Vector3 dir = Target - transform.position;
+                    //    //dir.y = 0;  // 평면상으로만 이동하려고 y = 0 했다
+                    //    dir.Normalize();
+                    //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.smoothDeltaTime * 4.0f);
+                    //}
+
+                    //myAnim.SetFloat("Speed", myNavAgent.velocity.magnitude / myNavAgent.speed);
+                    //myNavAgent.SetDestination(Target);
+                    break;
+                }
+            case STATE.SKILL:
+                {
+                    break;
+                }
+        }
+    }
+
+    public void OnAttack()
+    {
+
+    }
+}
--- RPG/Assets/Scripts/PlayerMovement.cs
+++ RPG/Assets/Scripts/PlayerMovement.cs
@@ -18,8 +18,6 @@ public class PlayerMovement : MonoBehaviour
 
     public Vector3 Target;
 
-    public float AttackTime;
-
     void Awake()
     {
         myAnim = GetComponent<Animator>();
@@ -27,30 +25,17 @@ public class PlayerMovement : MonoBehaviour
         myAnimEvent = GetComponent<PlayerAnimEvent>();
     }
 
-    private void Start()
-    {
-        myAnimEvent.Attack += OnAttack;
-    }
-
     void Update()
     {
         StateProcess();
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if(Physics.Raycast(ray, out hit, 999.0f, ClickMask))
             {
-                if(hit.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-                {
-                    Target = hit.point;
-                    ChangeState(STATE.BATTLE);
-                }
-                else
-                {
-                    Target = hit.point;
-                    ChangeState(STATE.NORMAL);
-                }
+                Target = hit.point;
+                ChangeState(STATE.NORMAL);
             }
         }
     }
@@ -70,11 +55,6 @@ public class PlayerMovement : MonoBehaviour
                 }
             case STATE.MOVE:
                 {
-                    break;
-                }
-            case STATE.BATTLE:
-                {
-                    AttackTime = 3.0f;
                     break;
                 }
         }
@@ -104,42 +84,6 @@ public class PlayerMovement : MonoBehaviour
                     myNavAgent.SetDestination(Target);
                     break;
                 }
-            case STATE.BATTLE:
-                {
-                    if (Target != Vector3.zero)
-                    {
-                        Vector3 dir = Target - transform.position;
-                        //dir.y = 0;  // 평면상으로만 이동하려고 y = 0 했다
-                        dir.Normalize();
-                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.smoothDeltaTime * 4.0f);
-                    }
-
-                    myAnim.SetFloat("Speed", myNavAgent.velocity.magnitude / myNavAgent.speed);
-                    myNavAgent.SetDestination(Target);
-
-                    if(AttackTime > 3.0f)
-                    {
-                        myAnim.SetTrigger("Attack");
-                        AttackTime = 0.0f;
-                    }
-                    else
-                    {
-                        AttackTime += Time.deltaTime;
-                    }
-                    break;
-                }
         }
     }
-
-    void OnAttack()
-    {
-        float distance = Vector3.Distance(transform.position, Target);
-        Vector3 dir = transform.position - Target;
-        float dot = Vector3.Dot(dir, transform.forward);
-        if(dot > 0 && distance < 3.0f)
-        {
-            Debug.Log("damage");
-        }
-    }
-
 }
